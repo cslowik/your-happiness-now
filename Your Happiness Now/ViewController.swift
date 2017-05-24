14d19d5d5dce5ec149838b43b1606eeb158da061
Chris Slowik <chris.slowik@gmail.com>
2017-05-23 21:53:07 -0700

    initial commit

---
+++ Your Happiness Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Your Happiness Now
+//
+//  Created by Chris Slowik on 5/23/17.
+//  Copyright © 2017 Six Voices. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
